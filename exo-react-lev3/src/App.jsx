5ab45e66e27bb84858d86849d487ae5dcf085b89
BENSALAHmyriam <mariem.bensalah@soprasteria.com>
2025-06-08 20:40:11 +0200

    vonfig gh page

--- exo-react-lev3/src/App.jsx
+++ exo-react-lev3/src/App.jsx
@@ -1,19 +1,20 @@
 import "./App.css";
-// import DialogDemo from "./exercices/dialog/component/DialogDemo";
+import DialogDemo from "./exercices/dialog/component/DialogDemo";
 import DropdownDemo from "./exercices/filter/DropDownDemo";
-//import ListenerComponent from "./exercices/localStorage/components/ListenerComponent";
-//import SetterComponent from "./exercices/localStorage/components/SetterComponent";
+import ListenerComponent from "./exercices/localStorage/components/ListenerComponent";
+import SetterComponent from "./exercices/localStorage/components/SetterComponent";
 
 export default function App() {
   return (
     <>
-      {/**    <div style={{ padding: 20 }}>
+      <div style={{ padding: 20 }}>
         <h1>Gestionnaire LocalStorage </h1>
         <SetterComponent />
         <ListenerComponent />
-      </div><div>
+      </div>
+      <div>
         <h1>Gestionnaire de modal </h1> <DialogDemo />
-      </div>*/}
+      </div>
 
       <div>
         <DropdownDemo />
